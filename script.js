f2f783d56d37341b7a067e0802c8bb016a475d8f
M H A Rashen Themiya Vishwajith <117666538+RashenThemiya@users.noreply.github.com>
2023-08-25 01:07:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-Certainly! To make your existing code work seamlessly for both touch screens and devices with mouse pointers, you can adapt the event listeners to support both types of interactions. Below is your code updated to handle both touch and mouse interactions:
-
-```javascript
 const keys = document.querySelectorAll('.key');
 const box = document.getElementById('box');
 const overlay = document.getElementById('overlay');
@@ -9,8 +6,8 @@ const backgroundMusic = document.getElementById('backgroundMusic');
 const musicControl = document.getElementById('musicControl');
 const musicIcon = document.getElementById('musicIcon');
 
-// Function to toggle music playback
-function toggleMusic() {
+
+musicControl.addEventListener('click', () => {
     if (backgroundMusic.paused) {
         backgroundMusic.play();
         musicIcon.innerText = '🎵';
@@ -18,108 +15,101 @@ function toggleMusic() {
         backgroundMusic.pause();
         musicIcon.innerText = '🔇';
     }
-}
+});
 
-// Toggle music on click (both touch and mouse)
-musicControl.addEventListener('click', toggleMusic);
-musicControl.addEventListener('touchstart', toggleMusic);
 
-// Function to handle drag start
-function handleDragStart(e) {
-    e.dataTransfer.setData('text/plain', this.dataset.aspect);
-}
 
-// Attach dragstart event to keys for both touch and mouse
+
+
 keys.forEach(key => {
-    key.addEventListener('dragstart', handleDragStart);
-    key.addEventListener('touchstart', handleDragStart);
+  key.addEventListener('dragstart', (e) => {
+    e.dataTransfer.setData('text/plain', key.dataset.aspect);
+  });
 });
 
-// Prevent default dragover behavior
 box.addEventListener('dragover', (e) => {
-    e.preventDefault();
+  e.preventDefault();
 });
 
-// Handle drop (both touch and mouse)
 box.addEventListener('drop', (e) => {
-    e.preventDefault();
-    const aspect = e.dataTransfer.getData('text/plain');
-    const draggedKey = document.querySelector(`[data-aspect="${aspect}"]`);
+  e.preventDefault();
+  const aspect = e.dataTransfer.getData('text/plain');
+  const draggedKey = document.querySelector(`[data-aspect="${aspect}"]`);
   
-    if (aspect && draggedKey) {
-        box.innerHTML = `<div class="treasure-open shrink"></div>`;
-        draggedKey.style.display = 'none';
-        checkAllKeysInBox();
-    }
+  if (aspect && draggedKey) {
+
+ 
+      box.innerHTML = `<div class="treasure-open shrink"> </div>`;
+      draggedKey.style.display = 'none';
+   
+      checkAllKeysInBox();
+      
+  
+
+  }
 });
 
-// Function to check all keys in box
 function checkAllKeysInBox() {
-    const remainingKeys = Array.from(keys).filter(key => key.style.display !== 'none');
+  const remainingKeys = Array.from(keys).filter(key => key.style.display !== 'none');
   
-    if (remainingKeys.length === 0) {
-        setTimeout(() => {
-            showWinPopup();
-        }, 1000);
-    }
+  if (remainingKeys.length === 0) {
+    setTimeout(() => {
+      showWinPopup();
+    }, 1000);
+  }
 }
 
-// Function to show the win popup
 function showWinPopup() {
-    overlay.style.display = 'flex';
-    popup.classList.add('animate-popup');
+  overlay.style.display = 'flex';
+  popup.classList.add('animate-popup');
 }
 
-// Attach animationend event to popup for both touch and mouse
 popup.addEventListener('animationend', () => {
-    popup.classList.remove('animate-popup');
+  popup.classList.remove('animate-popup');
 });
 
-// Countdown
+
+//count down
 (function () {
-    const second = 1000,
-          minute = second * 60,
-          hour = minute * 60,
-          day = hour * 24;
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
 
-    let today = new Date(),
-        dd = String(today.getDate()).padStart(2, "0"),
-        mm = String(today.getMonth() + 1).padStart(2, "0"),
-        yyyy = today.getFullYear(),
-        nextYear = yyyy + 1,
-        dayMonth = "08/31/",
-        criket = dayMonth + yyyy;
+  //I'm adding this section so I don't have to keep updating this pen every year :-)
+  //remove this if you don't need it
+  let today = new Date(),
+      dd = String(today.getDate()).padStart(2, "0"),
+      mm = String(today.getMonth() + 1).padStart(2, "0"),
+      yyyy = today.getFullYear(),
+      nextYear = yyyy + 1,
+      dayMonth = "08/31/",
+      criket = dayMonth + yyyy;
   
-    today = mm + "/" + dd + "/" + yyyy;
-    if (today > criket) {
-        criket = dayMonth + nextYear;
-    }
+  today = mm + "/" + dd + "/" + yyyy;
+  if (today > criket) {
+    criket = dayMonth + nextYear;
+  }
+  //end
   
-    const countDown = new Date(criket).getTime();
-    
-    // Countdown function
-    function updateCountdown() {
-        const now = new Date().getTime();
-        const distance = countDown - now;
+  const countDown = new Date(criket).getTime(),
+      x = setInterval(function() {    
 
-        document.getElementById("days").innerText = Math.floor(distance / (day));
-        document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour));
-        document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute));
-        document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+        const now = new Date().getTime(),
+              distance = countDown - now;
 
-        // Do something when date is reached
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+        //do something later when date is reached
         if (distance < 0) {
-            document.getElementById("headline").innerText = "It's my criket!";
-            document.getElementById("countdown").style.display = "none";
-            document.getElementById("content").style.display = "block";
-            clearInterval(countdownInterval);
+          document.getElementById("headline").innerText = "It's my criket!";
+          document.getElementById("countdown").style.display = "none";
+          document.getElementById("content").style.display = "block";
+          clearInterval(x);
         }
-    }
-
-    // Update countdown initially and every second
-    updateCountdown();
-    const countdownInterval = setInterval(updateCountdown, 1000);
-})();
-```
-
-This code should now work on both touch screens and devices with mouse pointers, adapting to the appropriate events for each interaction type. It covers touch events like `touchstart`, `touchmove`, and `touchend`, as well as mouse events like `click` and `dragstart`. Remember to test the code thoroughly on both touch and mouse-based devices to ensure smooth functionality.
+        //seconds
+      }, 0)
+  }());
